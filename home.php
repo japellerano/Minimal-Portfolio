b6a010436b5fb533578c5bbb666fb72ea2820c82
James Pellerano <james@japellerano.com>
2012-02-24 14:52:57 -0500

    update to home.php

--- home.php
+++ home.php
@@ -4,13 +4,15 @@
    <div id="content">
       <h2 id="recent">Recent Work</h2>
       
-      <?php $args = array('post_type' => 'project'); ?>
+      <?php $args = array('post_type' => 'project', 'posts_per_page' => 3); ?>
       <?php $loop = new WP_Query($args); ?>
       
       <?php while ($loop->have_posts()) : $loop->the_post(); ?>
          <article class="project">
-            <?php the_post_thumbnail(); ?>
-            <h3 class="project-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <a href="<?php the_permalink(); ?>">
+               <?php the_post_thumbnail(); ?>
+               <h3 class="project-title"><?php the_title(); ?></h3>
+            </a>
          </article>
       <?php endwhile; ?>
       <div class="clear"></div>
