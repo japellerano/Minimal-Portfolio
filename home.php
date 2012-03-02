5d1fd45bf7e69882f95e6375945d353df6053155
James Pellerano <james@japellerano.com>
2012-03-02 12:04:16 -0500

    Page Template
    Added generic page template
    added portfolio page template
    added styles and made the portfolio objects slightly smaller to be able
    to fit four across on the portfolio page

--- home.php
+++ home.php
@@ -14,7 +14,7 @@
                <a href="<?php the_permalink(); ?>">
                   <?php the_post_thumbnail(); ?>
                   <h3 class="project-title"><?php the_title(); ?></h3>
-                  <h5 class="project-category"><?php the_terms($post->ID, 'categoryportfolio', '', ', ', ' '); ?></h5>
+                  <h5 class="project-category"><?php the_terms($post->ID, 'categoryportfolio', '', ' ', ' '); ?></h5>
                </a>
             </article>
          <?php endwhile; ?>
---
+++ portfolio.php
@@ -0,0 +1,21 @@
+<?php /* Template Name: Portfolio */ ?>
+<?php get_header(); ?>
+
+<div id="content">
+   <div id="portfolio">
+      <?php $args = array('post_type' => 'project'); ?>
+      <?php $loop = new WP_Query($args); ?>
+      
+      <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+         <article class="project">
+            <a href="<?php the_permalink(); ?>">
+               <?php the_post_thumbnail(); ?>
+               <h3 class="project-title"><?php the_title(); ?></h3>
+               <h5 class="project-category"><?php the_terms($post->ID, 'categoryportfolio', '', ' ', ' '); ?></h5>
+            </a>
+         </article>
+      <?php endwhile; ?>
+   </div>
+</div>
+
+<?php get_footer(); ?>
