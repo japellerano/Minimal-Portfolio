4e4cdbee9c99f4bc9c13e4b43263ee4617ef7458
James Pellerano <james@japellerano.com>
2012-03-07 22:44:53 -0500

    single.php added
    needs more information

--- single.php
+++ single.php
@@ -1,3 +1,12 @@
 <?php get_header(); ?>
 
+   <div id="content">
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+         <h2 id="page" class="heading"><?php the_title(); ?></h2>
+         
+         <?php the_content(); ?>
+      <?php endwhile; endif; ?>
+      
+   </div>
+
 <?php get_footer(); ?>
