1ad41c069387370bba3cb65e153ee7169c5fc85d
James Pellerano <james@japellerano.com>
2012-03-08 02:49:57 -0500

    updated function name

--- functions/custom_wordpress.php
+++ functions/custom_wordpress.php
@@ -1,8 +1,8 @@
 <?php
 
-function remove_admin_footer()
+function replace_admin_text()
 {
 	echo '<span id="footer-thankyou">Developed by <a href="http://www.jamespellerano.com" target="_blank">James Pellerano</a></span>';
 }
-add_filter('admin_footer_text', 'remove_footer_admin');  
+add_filter('admin_footer_text', 'replace_admin_text');  
 ?>
