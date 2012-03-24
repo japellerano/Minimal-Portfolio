673b23a301b82c0998eba7668760d4f1039c9c5f
James Pellerano <james@japellerano.com>
2012-03-23 23:23:46 -0400

    reorganized css
    Reordered CSS file with indents

--- home.php
+++ home.php
@@ -20,7 +20,7 @@
          <?php endwhile; ?>
       </div>
       <div id="about">
-         <h2 class="heading">About Me</h2>
+         <h2 class="heading"></h2>
          <p id="front-page" class="medium-text"><?php echo get_option('artist_statement'); ?></p>
       </div>
       
