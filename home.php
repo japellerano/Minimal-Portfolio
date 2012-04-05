c46cc2bcb1caaedd89cbf7a30bd1d387429ec34f
James Pellerano <james@japellerano.com>
2012-04-05 00:39:58 -0400

    Home Page Update

--- home.php
+++ home.php
@@ -3,27 +3,23 @@
 
    <div class="clear"></div>
    <div id="content">
-      <div id="featured">
-         <h2 class="heading">Recent Work</h2>
-         
-         <?php $args = array('post_type' => 'project', 'posts_per_page' => 3); ?>
-         <?php $loop = new WP_Query($args); ?>
-         
-         <?php while ($loop->have_posts()) : $loop->the_post(); ?>
-            <article class="project">
-               <a href="<?php the_permalink(); ?>">
-                  <?php the_post_thumbnail('portfolio-small'); ?>
-                  <h3 class="project-title"><?php the_title(); ?></h3>
-                  <h5 class="project-category"><?php $terms_as_text = get_the_term_list($post->ID, 'categoryportfolio', '', ', ', ' '); if (!empty($terms_as_text)) echo strip_tags($terms_as_text); ?></h5>
-               </a>
-            </article>
-         <?php endwhile; ?>
-      </div>
-      <div id="about">
-         <h2 class="heading"><?php echo get_option('artist_statement_title'); ?></h2>
-         <p id="front-page" class="medium-text"><?php echo get_option('artist_statement'); ?></p>
-      </div>
-      
-      <div class="clear"></div>
-   </div>
+		<div class="container">
+			<div class="featured">
+				<?php $args = array('post_type' => 'project', 'posts_per_page' => 2); ?>
+				<?php $loop = new WP_Query($args); ?>
+				
+				<?php while ($loop->have_posts()) : $loop->the_post(); ?>
+					<article class="project featured">
+						<?php the_post_thumbnail('portfolio-small'); ?>
+						<h2 class="project-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+						<h5 class="project-category"><?php the_terms($post->ID, 'categoryportfolio', '', ' ', ' '); ?></h5>
+						
+						<?php the_excerpt(); ?>
+						<a class="read-more" href="<?php the_permalink(); ?>">See More</a>
+					</article>
+				<?php endwhile; ?>
+			</div>   	
+			<div class="clear"></div>
+		</div>
+	</div>
 <?php get_footer(); ?>
