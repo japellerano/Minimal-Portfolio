597c101ef3844d3ac118c3f1b552e777c17e3d39
James Pellerano <james@japellerano.com>
2012-04-06 15:26:41 -0400

    Blog Template

--- sidebar.php
+++ sidebar.php
@@ -1,14 +1,14 @@
 <ul id="sidebar-items">
    <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar()) : ?>
       <li id="sidebar-about">
-         <h3 class="heading sidebar">About</h3>
+         <h2 class="widgettitle sidebar">About</h3>
          <p>This is my blog</p>
       </li>
    <?php endif; ?>
 </ul>
 
 <div id="twitter">
-   <h2 class="widgettitle">Twitter Feed</h2>
+   <h2 class="widgettitle sidebar">Twitter Feed</h2>
    <?php
       include_once(ABSPATH . WPINC . '/feed.php');
       $rss = fetch_feed('https://api.twitter.com/1/statuses/user_timeline.rss?screen_name=japellerano');
